d4e9d9fa3e20fd735e5cb5fa867f0fb3b28629c6
Zakai Ikar <zakariyeiikar669@gmail.com>
2025-02-03 16:22:07 +0300

    adding evevent handler to Functional Componenent

--- myapp/src/Component/FunctionalComponent.jsx
+++ myapp/src/Component/FunctionalComponent.jsx
@@ -2,11 +2,20 @@ import React from "react"
 
 const FuntionalComponent=()=>{
     const [data, SetData] = React.useState("react");
-    console.log(data);
+    // setstate(Vue)
+    // const [data] = React.useState("react");
+    // console.log(data);
+    const handleclick=()=>{
+        console.log("this is click event");
+        SetData("vue")
+    }
     return (
  
         <div>
             <p> this  is functional componenent </p>
+           
+            <button type="button" onClick={handleclick}>click me</button>
+            <p>State: {data}</p>
         </div>
     )
 }
